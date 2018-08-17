fa59ddf3322d11e58af06ea6e63c734fdf2ea231
Dominik Froehlich <dominik.froehlich@sap.com>
2018-08-17 14:01:03 +0200

    add hostname initial message

--- echo/main.go
+++ echo/main.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	//A simple echo server
 
-	http.HandleFunc("/", handleDefault)
+	hostname, _ := os.Hostname()
 
+	fmt.Printf("Starting echo server on %s\n", hostname)
+
+	http.HandleFunc("/", handleDefault)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handleDefault(w http.ResponseWriter, r *http.Request) {
 
-	buf := new([]byte)
+	buf, err := ioutil.ReadAll(r.Body)
 
-	io.ReadFull(r.Body, *buf)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		fmt.Fprintf(w, "ERROR: %s\n", err)
+		return
+	}
 
-	answer := string(*buf)
-
+	answer := string(buf)
 	fmt.Printf("ECHO: %s\n", answer)
 	fmt.Fprintf(w, "%s\n", answer)
 }
